ff3fd642211f01222d8376907eaa2807268e74d5
456456-c <1753340693@qq.com>
2025-09-20 23:07:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# palindrome.py
-
 num = input().strip()
 
 if not num.isdigit() or len(num) != 5:
-    print("错误")
+    print("错误提示")
+elif num == num[::-1]:
+    print("是回文数")
 else:
-    if num == num[::-1]:
-        print("是回文数")
-    else:
-        print("不是回文数")
+    print("不是回文数")
 
