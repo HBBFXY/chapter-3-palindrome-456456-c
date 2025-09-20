349e4f952174582714f179eae2a1e276f6242513
456456-c <1753340693@qq.com>
2025-09-20 23:08:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# main.py
+
 num = input().strip()
 
+# 非纯数字或不是5位数
 if not num.isdigit() or len(num) != 5:
-    print("错误提示")
+    print("输入错误: 请输入5位数字")
 elif num == num[::-1]:
     print("是回文数")
 else:
     print("不是回文数")
-
