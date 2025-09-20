1c1c4c0c6d1943827a1b9913eed61fc0d51d344d
456456-c <1753340693@qq.com>
2025-09-20 23:05:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# 回文数判断程序
+# palindrome.py
 
-# 从键盘输入
-num = input("请输入一个5位数字：")
+num = input().strip()
 
-# 1. 判断是否为纯数字
-if not num.isdigit():
-    print("错误：输入必须是纯数字")
-# 2. 判断是否为5位
-elif len(num) != 5:
-    print("错误：输入必须是一个5位数字")
+if not num.isdigit() or len(num) != 5:
+    print("错误")
 else:
-    # 3. 判断是否为回文数（字符串翻转比较）
     if num == num[::-1]:
         print("是回文数")
     else:
